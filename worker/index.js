5e696a959eeb00c0cf04f7a7cff9ddbcdd558af8
root <root@kali.kali>
2021-08-28 22:38:20 +0100

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,24 @@
+"use strict";
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => (1000),
+})
+
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+    if(index < 2) return 1;
+    return fib(index -1) + fib(index - 2);
+}
+
+// message variable is our index to calculate, sent to us by server after publish
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)))
+});
+// insert is our identifier
+sub.subscribe('insert');
