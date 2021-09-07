804dab30e8e68aa12eb358d86a81f709303b6358
root <root@kali.kali>
2021-09-07 01:18:05 +0100

    changed memory term to memoryReservation

--- server/index.js
+++ server/index.js
@@ -60,7 +60,6 @@ app.get('/values/redis', (req, res) => {
 })
 
 app.post('/values', (req, res) => {
-    alert('post executed');
     const index = req.body.index;
     if (parseInt(index) > 40) {
         return res.status(422).send('Index too high');
